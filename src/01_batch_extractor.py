2a67bb525f340bb1852fbdcdc6006c9a7b692179
Shailya777 <shailya.gandhi100@gmail.com>
2026-07-06 06:27:43 +0000

    Function to serialize single building's data into tf.train.Example message

--- src/01_batch_extractor.py
+++ src/01_batch_extractor.py
@@ -173,8 +173,44 @@ def load_and_crop_image(image_path: tf.Tensor, ymin: tf.Tensor, xmin: tf.Tensor,
 
     return preprocessed_image
 
+def serialize_example(building_id: tf.Tensor, feature_vector: tf.Tensor, label: tf.Tensor) -> bytes:
+    """
+    Serializes a single building's extracted data into a tf.train.Example message.
+
+    Args:
+        building_id (tf.Tensor): A string tensor containing the unique building identifier.
+        feature_vector (tf.Tensor): A float32 tensor of shape (2048,) representing the 
+            extracted ResNet50 features.
+        label (tf.Tensor): A string tensor containing the xBD damage classification label.
+
+    Returns:
+        bytes: A serialized tf.train.Example proto string ready to be written to a TFRecord.
+    """
+
+    # Helper Functions to Format data for TFRedords:
+    def _bytes_feature(value):
+        if isinstance(value, type(tf.constant(0))):
+            value= value.numpy()
+        return tf.train.Feature(bytes_list= tf.train.BytesList(value= [value]))
+
+    def _float_feature(value):
+        return tf.train.Feature(float_list= tf.train.FloatList(value= value))
+    
+    feature= {
+        'building_id': _bytes_feature(building_id.numpy()),
+        'label': _bytes_feature(label.numpy()),
+        'feature_vector': _float_feature(feature_vector.numpy().flatten().tolist())
+    }
+
+    examplt_proto= tf.train.Example(features= tf.train.Features(feature= feature))
+    return examplt_proto.SerializeToString()
+
 if __name__ == '__main__':
     #a,b,c,d= extract_bbox_from_wkt('POLYGON ((-90.81544679490855 14.39086318334812, -90.81537467350067 14.39060467857134, -90.81584174451893 14.39043032647906, -90.81586635209965 14.39049581582557, -90.81593344431286 14.39048145754227, -90.81595559689623 14.39057367091926, -90.81587964155047 14.39059650626524, -90.81590706308843 14.39071123556855, -90.81544679490855 14.39086318334812))')
     #print(a, b, c, d)
 
-    build_metadata_csv()
+    #build_metadata_csv()
+    temp_str= serialize_example(building_id= tf.constant('buidling_101'),
+                                feature_vector= tf.constant([0.23, 0.81, 0.17], dtype= tf.float32),
+                                label= tf.constant('destroyed'))
+    print(temp_str)
