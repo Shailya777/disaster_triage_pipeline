f59acafbc93047bd7431c79cbbdcb78b92e1e04d
Shailya777 <shailya.gandhi100@gmail.com>
2026-07-09 05:33:46 +0000

    Initializing and Loading ResNet50 and XGBoost model

--- src/04_shocase_prep.py
+++ src/04_shocase_prep.py
@@ -12,3 +12,22 @@ PROJECT_ROOT= os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SHOWCASE_DIR= os.path.join(PROJECT_ROOT, 'data', 'showcase')
 MODEL_JSON_PATH= os.path.join(PROJECT_ROOT, 'models', 'xgboost_classifier.json')
 OUTPUT_CSV_PATH= os.path.join(PROJECT_ROOT, 'data', 'processed', 'showcase.csv')
+
+def load_pipeline():
+    """
+    Initializes both the deep learning extractor and the XGBoost classifier.
+    """
+
+    # Loading ResNet50:
+    print('Loading Frozen ResNet50 Feature Extractor...')
+    resnet= tf.keras.applications.ResNet50(
+        include_top= False,
+        weights= 'imagenet',
+        pooling= 'avg'
+    )
+
+    # Loading XGBoost Classifier:
+    xgb_model= xgb.XGBClassifier()
+    xgb_model.load_model(fname= MODEL_JSON_PATH)
+
+    return resnet, xgb_model
