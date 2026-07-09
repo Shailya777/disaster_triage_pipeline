4c659110c3582f7a4a36618636bc427e325700a1
Shailya777 <shailya.gandhi100@gmail.com>
2026-07-09 06:21:27 +0000

    Extracting bounding box of buildings from image

--- src/04_shocase_prep.py
+++ src/04_shocase_prep.py
@@ -43,3 +43,73 @@ def load_pipeline():
     xgb_model.load_model(fname= MODEL_JSON_PATH)
 
     return resnet, xgb_model
+
+def extract_real_buidings(image_name):
+    """
+    Parses the xBD JSON label file to extract true building bounding boxes.
+
+    This function locates the corresponding JSON label file for a given image,
+    extracts the WKT (Well-Known Text) polygon coordinates for every structure,
+    and calculates a Cartesian bounding box (ymin, xmin, ymax, xmax).
+
+    Args:
+        image_name (str): The filename of the image (e.g., 'hurricane-michael_001.png').
+
+    Returns:
+        list of dicts: A list containing building metadata and coordinates. 
+                       Format: [{'uid': str, 'box': (ymin, xmin, ymax, xmax)}, ...]
+    """
+
+    # Image Path fromImage Name:
+    json_name= image_name.replace('.png', '.json').replace('.jpg', '.json')
+    json_path= os.path.join(LABEL_DIR, json_name)
+
+    if not os.path.exists(json_path):
+        return []
+    
+    buildings= []
+    with open(json_path, 'r') as f:
+        data= json.load(f)
+
+    # Getting Polygon Features from JSON File:
+    features= data.get('features', {}).get('xy', [])
+
+    # Getting Bounding Box Coordinates from features:
+    for feature in features:
+        uid= feature.get('properties', {}).get('uid', 'unknown')
+        wkt_string= feature.get('wkt', '')
+
+        # Getting POLYGON numbers from wkt. Format: POLYGON ((x1 y1, x2 y2, x3 y3...)):
+        match= re.search(r'\(\((.*?)\)\)', wkt_string)
+        if not match:
+            continue
+
+        coords_string= match.group(1)
+        points= coords_string.split(',')
+
+        x_coords= []
+        y_coords= []
+
+        for point in points:
+            coords= point.strip().split(' ')
+            if len(coords) == 2:
+                x_coords.append(float(coords[0]))
+                y_coords.append(float(coords[1]))
+
+        # Calculating Bounding Box from the Coordinates:
+        xmin, xmax=  int(min(x_coords)), int(max(x_coords))
+        ymin, ymax = int(min(y_coords)), int(max(y_coords))
+
+        # ensuring Coordinates are Mathematically Valid (Preventing 0-pixel Crops):
+        if xmax > xmin and ymax > ymin:
+            buildings.append({
+                'uid': uid,
+                'box': (ymin, xmin, ymax, xmax)
+            })
+        
+    return buildings
+
+if __name__ == '__main__':
+    temp= extract_real_buidings('palu-tsunami_00000064_post_disaster.png')
+    print(temp)
+    print(len(temp))
