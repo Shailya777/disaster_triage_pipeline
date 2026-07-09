3fdbe5eef2e351ff21362091c547ed3dacf67627
Shailya777 <shailya.gandhi100@gmail.com>
2026-07-09 12:58:57 +0000

    Slider for Selecting Threshold

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 from PIL import Image
 import streamlit as st
 
-# Page Configuration:
+# 1. Page Configuration:
 st.set_page_config(
     page_title= 'Disaster Triage AI',
     page_icon= '🛰️',
@@ -14,7 +14,7 @@ st.set_page_config(
     initial_sidebar_state= 'expanded'   
 )
 
-# Dark Mode Aesthetic:
+## Dark Mode Aesthetic:
 st.markdown("""
             <style>
     .stApp { background-color: #0E1117; color: #FAFAFA; }
@@ -23,11 +23,11 @@ st.markdown("""
             """,
             unsafe_allow_html= True)
 
-# Defining Directory Paths:
+# 2. Defining Directory Paths:
 SHOWCASE_DIR= 'data/showcase'
 SHOWCASE_PATH= 'data/processed/showcase.csv'
 
-# Loading Showcase CSV:
+# 3. Loading Showcase CSV:
 @st.cache_data
 def load_data():
     return pd.read_csv(SHOWCASE_PATH)
@@ -38,7 +38,7 @@ except FileNotFoundError:
     st.error(f'Critical Error: Could not find Showcase file at {SHOWCASE_PATH}.')
     st.stop()
 
-# Sidebar Panel Configuration:
+# 4. Sidebar Panel Configuration:
 st.sidebar.title('Triage Command')
 st.sidebar.markdown('---')
 
@@ -47,6 +47,13 @@ df['disaster_name']= df['image_name'].apply(lambda x: x.split('_')[0].replace('-
 disaster_list= df['disaster_name'].unique()
 selected_disaster= st.sidebar.selectbox('Select Event:', disaster_list)
 
-# Filtering Images based on Selected Disaster:
+## Filtering Images based on Selected Disaster:
 filtered_images= df[df['disaster_name'] == selected_disaster]['image_name'].unique()
 selected_image= st.sidebar.selectbox('Intercept Satellite Feed:', filtered_images)
+
+## Slider for Selecting Threshold:
+st.sidebar.markdown('---')
+st.sidebar.subheader('Triage Thresholds')
+
+destroyed_thresh= st.sidebar.slider('Destroyed Confidence Threshold', min_value= 0.30, max_value= 0.99, value= 0.50, step= 0.05)
+st.sidebar.caption('Highlight structures with a high probability of total collapse.')
