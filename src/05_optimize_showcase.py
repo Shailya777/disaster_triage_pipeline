2c41e86da42e2c111b5b0f07ce3d26a34dad4637
Shailya777 <shailya.gandhi100@gmail.com>
2026-07-13 12:54:56 +0000

    Func to update showcase csv with .jpg in image_name instead of .png, converted images and updated CSV

--- src/05_optimize_showcase.py
+++ src/05_optimize_showcase.py
@@ -9,7 +9,7 @@ PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SHOWCASE_DIR= os.path.join(PROJECT_DIR, 'data', 'showcase')
 CSV_PATH= os.path.join(PROJECT_DIR, 'data', 'processed', 'showcase.csv')
 
-def optimize_images():
+def convert_images():
     """
     Takes all Images from Showcase Directory and converts from .png to .jpeg.
     Deletes original .png files after conversion.
@@ -33,7 +33,7 @@ def optimize_images():
         try:
             img= Image.open(png_path)
             rgb_img= img.convert(mode= 'RGB')
-            rgb_img.save(fp= jpg_path, format= 'JPEG', quaulity= 85)
+            rgb_img.save(fp= jpg_path, format= 'JPEG', quaulity= 90)
 
             # Deleting the Original .png file:
             os.remove(png_path)
@@ -43,3 +43,23 @@ def optimize_images():
             print(f'Error Processing {png_path}: {e}')
 
     print(f'Successfully Converted {success_count} Images.')
+
+def update_showcase_csv():
+    """
+    Updates Showcase CSV file to have image_name column .jpg instead of .png.
+    """
+    print('Updating Showcase CSV...')
+    if not os.path.exists(CSV_PATH):
+        print(f'Error: Could Not Find {CSV_PATH}')
+        return
+    
+    df= pd.read_csv(CSV_PATH)
+
+    # Replacing .png with .jpg in image_name column:
+    df['image_name']= df['image_name'].str.replace('.png', '.jpg')
+    df.to_csv(path_or_buf= CSV_PATH, index= False)
+    print('Showcase CSV Successfully Updated.')
+
+if __name__ == '__main__':
+    convert_images() # Converts All Showcase Images form .png to .jpg, deleting original .png files after successful conversion.
+    update_showcase_csv() # Updates Showcase CSV file to have .jpg instead of .png in image_name column.
